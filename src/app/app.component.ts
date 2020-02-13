348616922810cf77a1922445478145e3473f586a
MaaElattar <maaelattar@gmail.com>
2020-02-13 03:39:11 +0200

    Add app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './core/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'u-blog';
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.userService.populate();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRoutingModule,
+    AuthModule,
+    HomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
